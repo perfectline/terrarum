be04d5f5c90b92b53a67be406fdf7be8050b7232
PerfectLine <info@perfectline.ee>
2010-11-23 02:49:07 -0800

    - fix typo

--- lib/generators/countries/templates/model.rb
+++ lib/generators/countries/templates/model.rb
@@ -3,6 +3,6 @@ class <%= class_name %> < <%= parent_class_name.classify %>
   scope :code, lambda { |code| where(:code => code.to_s) }
 
   validates :code, :presence => true, :length => { :maximum => 5 }, :uniqueness => true
-  validates :name, :presence => true, :lenght => { :maximum => 100 }
+  validates :name, :presence => true, :length => { :maximum => 100 }
 
 end
